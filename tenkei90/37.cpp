13de9a168b9c53b518a6fbb13f7702ef0c288be6
naru-19 <narururu19@gmail.com>
2022-12-07 23:14:40 +0900

    add tenkei37(dp復元)

--- tenkei90/37.cpp
+++ tenkei90/37.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
+
 #include <atcoder/all>
 using namespace std;
 #define rep(i, n) for (ll i = 0; i < n; ++i)
-#define rep2(i,j,n) for(ll i=j;i<n;++i)
+#define rep2(i, j, n) for (ll i = j; i < n; ++i)
 #define Sort(a) sort(a.begin(), a.end())
 #define Sortr(a) sort(a.rbegin(), a.rend())
 using ll = long long;
@@ -10,137 +11,50 @@ using ll = long long;
 #define INF 1001001001001001000
 #define yes "Yes"
 #define no "No"
-template <class T>
-inline bool chmin(T &a, T b){
-    if (a > b){
-        a = b;return true;
-    }
-    return false;
-}
-template <class T>
-inline bool chmax(T &a, T b){
-    if (a < b){
-        a = b;return true;
-    }
-    return false;
-}
 
-struct edge{ll to, cost;};
-typedef pair<ll,ll> P;
-struct graph{
-    ll V;
-    vector<vector<edge>> G;
-    vector<ll> d;
-    graph(ll n){
-        init(n);
-    }   
-    void init(ll n){
-        V = n;
-        G.resize(V);
-        d.resize(V);
-        rep(i,V){
-            d[i] = INF;
-        }
+int main() {
+  ll w, n;
+  cin >> w >> n;
+  vector<vector<ll>> spice(n, vector<ll>(3));
+  rep(i, n) { cin >> spice[i][0] >> spice[i][1] >> spice[i][2]; }
+
+  // まずは下限を使ってdpを行う。
+  vector<vector<ll>> dp(n + 1, vector<ll>(w + 1, -INF));
+  dp[0][0] = 0;
+  rep2(i, 1, n + 1) {
+    rep(_w, w + 1) {
+      dp[i][_w] = max(dp[i - 1][_w], dp[i][_w]);
+      if (_w + spice[i - 1][0] > w) continue;
+      if (dp[i - 1][_w] == -INF) continue;
+      dp[i][_w + spice[i - 1][0]] =
+          max(dp[i][_w + spice[i - 1][0]], dp[i - 1][_w] + spice[i - 1][2]);
     }
-  void add_edge(ll s, ll t, ll cost){
-    edge e;
-    e.to = t, e.cost = cost;
-    G[s].push_back(e);
   }
-
-  void dij(ll s){
-    rep(i,V){
-      d[i] = INF;
-    }
-    d[s] = 0;
-    priority_queue<P,vector<P>, greater<P> > que;
-    que.push(P(0,s));
-    while(!que.empty()){
-      P p = que.top(); que.pop();
-      ll v = p.second;
-      if(d[v]<p.first) continue;
-      for(auto e : G[v]){
-        if(d[e.to]>d[v]+e.cost){
-          d[e.to] = d[v]+e.cost;
-          que.push(P(d[e.to],e.to));
-        }
+  // V、下限をVでソート
+  vector<vector<ll>> lowerlimit(w + 1, vector<ll>(2));
+  rep(i, w + 1) {
+    lowerlimit[i][0] = dp[n][i];
+    lowerlimit[i][1] = i;
+  }
+  Sortr(lowerlimit);
+  ll ans = -1;
+  rep(i, w + 1) {
+    // 復元
+    ll tmp = lowerlimit[i][0];
+    if (tmp == -INF) continue;
+    ll l = lowerlimit[i][1];
+    ll r = 0;
+    for (ll j = n - 1; j >= 0; j--) {
+      if (dp[j][l - spice[j][0]] == tmp - spice[j][2]) {
+        r += spice[j][1];
+        tmp -= spice[j][2];
+        l -= spice[j][0];
       }
     }
-  }
-};
-void keta(double x){cout<<fixed<<setprecision(10)<<x;}
-string num2str(ll x){
-    string s;ostringstream oss;oss <<x<<flush;s = oss.str();
-    return s;
-}
-#define DEBUG_ON
-
-
-
-
-ll modinv(ll a, ll MOD) {
-    ll m= MOD;
-    ll b = m, u = 1, v = 0;
-    while (b) {
-        ll t = a / b;
-        a -= t * b; swap(a, b);
-        u -= t * v; swap(u, v);
-    }
-    u %= m; 
-    if (u < 0) u += m;
-    return u;
-}
-class UnionFind {
-private:
-//   int* data;
-    vector<ll>data;
-
-public:
-  UnionFind(int n = 0) : data(n, -1) {}
-
-  int find(int x) {
-    return data[x] < 0 ? x : data[x] = find(data[x]);
-  }
-
-  bool unite(int x, int y) {
-    x = find(x); y = find(y);
-
-    if (x == y) return false;
-    else {
-      if(data[x] > data[y]) swap(x, y);
-      data[x] += data[y];
-      data[y] = x;
-      return true;
+    if (r >= w) {
+      cout << lowerlimit[i][0] << endl;
+      return 0;
     }
   }
-
-  bool is_same(int x, int y) {
-    return find(x) == find(y);
-  }
-
-  int size(int x) {
-    return -data[find(x)];
-  }
-};
-ll MOD = 1000000007;
-// ll MOD=998244353;
-long long pow2(long long x, long long n) {long long ret = 1;    while (n > 0) {        if (n & 1) ret = ret * x % MOD;  // n の最下位bitが 1 ならば x^(2^i) をかける
-        x = x * x % MOD;
-        n >>= 1;  // n を1bit 左にずらす
-    }
-    return ret;
-}
-
-ll ncr(ll n,ll r){ll ans=1;
-    rep(i,r){ans=ans*(n-i);ans*=pow2(i+1,MOD-2);ans%=MOD;}
-    return ans;
-}
-// ll h, w;
-// ll n;
-// ll n;
-// cin>>n;
-// string s(n,'');
-int main()
-{
-
+  cout << -1 << endl;
 }
